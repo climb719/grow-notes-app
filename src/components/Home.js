f325f9e087c63549b7d4c17cf5b19659f5149a05
climb719 <mary.oldweiler@gmail.com>
2022-01-22 11:16:06 -0500

    add router, nav, home

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+const Home = props => {
+    return (
+     <div className="home">
+   
+     <h1 className="page-heading">Grow Notes</h1>
+    
+    
+     
+     </div>
+    )
+}
+
+export default Home
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
+const Nav = props => {
+    return (
+     <div className="nav-bar">
+     <ul>
+      <li><Link end to="/"><div className='link'>Home</div></Link></li>
+      <li><Link end to="/scheduling"><div className='link'>Scheduling</div></Link></li>
+      <li><Link to="/to-do"><div className='link'>To-Do</div></Link></li>
+      <li><Link to="/resources"><div className='link'>Resources</div></Link></li>
+      <li><Link to="/clients"><div className='link'>Clients</div></Link></li>
+     </ul>
+
+      </div>
+      )
+    }
+    
+export default Nav;
