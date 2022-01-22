a9b131abebe652b3fecd973808d7f2c1b25faa5b
climb719 <mary.oldweiler@gmail.com>
2022-01-22 16:42:14 -0500

    test text pop up with icons

--- src/containers/Clients.js
+++ src/containers/Clients.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import AsyncSelect from 'react-select/async';
-import { clientNames } from './data';
+
 
 
 
 class Clients extends Component {
 
   
-    
-
     render() {
      return ( 
         <div className="clients">
         <h3 className="page-heading">Client Options</h3>
         <ul className= "icons">
-        <li><img src="images/001-documents.png" alt="intake forms"/></li>
+        <li><img src="images/001-documents.png" alt="intake forms"/>
+        <p className="content">Intake Forms</p></li>
         <li><img src="images/005-mental-health.png" alt="holding heart and mind"/></li>
         <li><img src="images/002-solution.png" alt="mind with puzzle"/></li>
         <li><img src="images/006-brain.png" alt="heart and brain"/></li>
