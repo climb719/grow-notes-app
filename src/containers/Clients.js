f6c5defe8b8b3ad22407bea50ee6ca947866ee98
climb719 <mary.oldweiler@gmail.com>
2022-01-22 16:28:43 -0500

    add css and client form icons

--- src/containers/Clients.js
+++ src/containers/Clients.js
@@ -1,14 +1,29 @@
-const Clients = props => {
-    return (
-     <div className="slients">
-   
-     <h1 className="page-heading">Clients</h1>
-     <p> </p>
+import React, { Component } from 'react';
+import AsyncSelect from 'react-select/async';
+import { clientNames } from './data';
+
+
+
+class Clients extends Component {
+
+  
     
-    
-     
-     </div>
-    )
+
+    render() {
+     return ( 
+        <div className="clients">
+        <h3 className="page-heading">Client Forms</h3>
+        <ul className= "icons">
+        <li><img src="images/001-documents.png" alt="intake forms"/></li>
+        <li><img src="images/005-mental-health.png" alt="holding heart and mind"/></li>
+        <li><img src="images/002-solution.png" alt="mind with puzzle"/></li>
+        <li><img src="images/006-brain.png" alt="heart and brain"/></li>
+        </ul>
+       
+        </div>
+     )
+    }
 }
+
 
 export default Clients
---
+++ src/containers/data.js
@@ -0,0 +1,7 @@
+export const clientNames = [
+    { name: "Mousey Mouse"},
+    { name: "Doggy Dog"},
+    { name: "Ducky Duck"},
+    { name: "Kitty Cat"},
+
+]
